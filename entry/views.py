0a60fdac045f2d68cc8631027a79edad0dccf032
ugik <georgek@gmail.com>
2012-06-15 03:59:54 -0400

    tropo

--- entry/views.py
+++ entry/views.py
@@ -20,7 +20,7 @@ def hello(request):
     
     try:
         s = Session(request.body)
-        print s.fromaddress
+        print('Cell #%s' % s.fromaddress.dict['id'])
         return HttpResponse(json) 
     except Exception, err:
         print('ERROR: %s\n' % str(err))
