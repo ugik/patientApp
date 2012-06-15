0a1ac8c428438486711c219427cc1ee421413884
ugik <georgek@gmail.com>
2012-06-15 03:15:55 -0400

    tropo

--- entry/views.py
+++ entry/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render_to_response
 from django.template import RequestContext
-from django.http import HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login, logout
