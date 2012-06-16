3b7909e6bb08e4dbc3ac5a1eaed9437256a55ae4
ugik <georgek@gmail.com>
2012-06-16 12:30:07 -0400

    cell processing

--- entry/views.py
+++ entry/views.py
@@ -15,12 +15,28 @@ from django.views.decorators.csrf import csrf_exempt
 def hello(request):
     t = Tropo()
     msg = request.POST['msg']
-    json = t.say("you just said: " + msg)
-    json = t.RenderJson(json)
-    
-    try:
+
+        try:
         s = Session(request.body)
         print('Cell #%s' % s.fromaddress['id'])
+
+# lookup patient with this cell #
+        cell = s.fromaddress['id']
+        if cell[0] is '1':   # trim leading 1 in cell # if there
+            cell = cell[1:]
+        p = Patient.objects.filter(cell=cell)   # all patients with this cell #
+        if p.exists():                                    # if cell # found then create new entry
+            if p.count>1
+                print('WARNING: Multiple patients with cell # %s' % cell)
+            parent = p[0]  # assume first 
+            entry = Entry(patient=parent, entry=msg)
+            entry.save()
+            json = t.say("Entry saved, thank you " + parent.name)
+            json = t.RenderJson(json)
+        else:                                               # if cell # NOT found then notify
+            json = t.say("Could not find patient with cell # " + cell)
+            json = t.RenderJson(json)
+        
         return HttpResponse(json) 
     except Exception, err:
         print('ERROR: %s\n' % str(err))
