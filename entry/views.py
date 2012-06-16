97d1bb32de848fbd4adc5a555af501a5b4d62790
ugik <georgek@gmail.com>
2012-06-16 12:46:33 -0400

    cell processing

--- entry/views.py
+++ entry/views.py
@@ -20,10 +20,30 @@ def hello(request):
     
     try:
         s = Session(request.body)
-        print('Cell #%s' % s.fromaddress['id'])
+        cell = s.fromaddress['id']
+        print('Cell #%s' % cell)
+
+# lookup patient with this cell #
+        if cell[0] is '1':   # trim leading 1 in cell # if there
+            cell = cell[1:]
+        p = Patient.objects.filter(cell=cell)   # all patients with this cell #
+        if p.exists():                                    # if cell # found then create new entry
+            if p.count>1
+                print('WARNING: Multiple patients with cell # %s' % cell)
+            parent = p[0]  # assume first 
+            entry = Entry(patient=parent, entry=msg)
+            entry.save()
+            json = t.say("Entry saved, thank you " + parent.name)
+            json = t.RenderJson(json)
+        else:                                               # if cell # NOT found then notify
+            json = t.say("Could not find patient with cell # " + cell)
+            json = t.RenderJson(json)
+
         return HttpResponse(json)
     except Exception, err:
         print('ERROR: %s\n' % str(err))
+
+
 
 def HomePage(request):
     context = {}
